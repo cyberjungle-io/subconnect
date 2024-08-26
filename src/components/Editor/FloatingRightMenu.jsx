df783d501b12fd5beef604088fb406d6bd7dde3d
James Garcia <james@cyberjungle.io>
2024-08-26 10:14:26 -0700

    CHanged icons and added hide visibility to the floating right menu

--- src/components/Editor/FloatingRightMenu.jsx
+++ src/components/Editor/FloatingRightMenu.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { FaTree, FaCog, FaEye, FaChevronLeft, FaHandPointer } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaTree, FaGlobe, FaEye, FaHandPointer, FaChevronLeft } from 'react-icons/fa';
+import HidePropertiesPanelArrow from '../common/CustomIcons/HidePropertiesPanelArrow';
 
 // Add this new custom icon component
 const FourSquaresIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="1em" height="1em">
-    <path d="M10 3H3v7h7V3zm11 0h-7v7h7V3zm0 11h-7v7h7v-7zm-11 0H3v7h7v-7z" />
+    <path d="M10 3H3v7h7V3zm11 0h-7v7h7V3zm0 11h-7v7h7v-7z" />
   </svg>
 );
 
@@ -20,6 +21,24 @@ const FloatingRightMenu = ({
   isGlobalSettingsVisible,
   isDragModeEnabled,
 }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
+  if (!isVisible) {
+    return (
+      <button
+        onClick={toggleVisibility}
+        className="fixed right-0 top-1/3 transform -translate-y-1/2 bg-[#e6f3ff] border border-[#b3d9ff] rounded-l-lg shadow-xl z-[960] p-2"
+        title="Show Menu"
+      >
+        <FaChevronLeft />
+      </button>
+    );
+  }
+
   return (
     <div className="fixed right-8 top-1/3 transform -translate-y-1/2 w-12 bg-[#e6f3ff] border border-[#b3d9ff] rounded-lg shadow-xl z-[960] flex flex-col items-center py-2">
       <button
@@ -41,7 +60,7 @@ const FloatingRightMenu = ({
         className={`p-2 mb-2 rounded-full ${isGlobalSettingsVisible ? 'bg-[#cce7ff] text-blue-600' : 'hover:bg-[#d9ecff]'}`}
         title="Toggle Global Settings"
       >
-        <FaCog />
+        <FaGlobe />
       </button>
       <button
         onClick={onToggleDragMode}
@@ -51,7 +70,9 @@ const FloatingRightMenu = ({
         <FaHandPointer />
       </button>
       <button onClick={onToggleSpacingVisibility} className="p-2 mb-2 rounded-full hover:bg-[#d9ecff]" title="Toggle Spacing Visibility"><FaEye /></button>
-      <button onClick={onToggleVisibility} className="p-2 mb-2 rounded-full hover:bg-[#d9ecff]" title="Toggle Menu Visibility"><FaChevronLeft /></button>
+      <button onClick={toggleVisibility} className="p-2 mb-2 rounded-full hover:bg-[#d9ecff]" title="Hide Menu">
+        <HidePropertiesPanelArrow className="w-5 h-5" />
+      </button>
     </div>
   );
 };
