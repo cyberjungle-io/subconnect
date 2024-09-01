690afb085a50a163534f2ff024bb39dbf0019243
Jim Garcia <jim@cyberjungle.io>
2024-09-01 15:00:20 -0700

    update

--- src/components/Components/Renderers/QueryValueRenderer.jsx
+++ src/components/Components/Renderers/QueryValueRenderer.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchQuery } from '../../../w3s/w3sSlice';
+import { executeQuery } from '../../../features/graphQLSlice';
 
 const QueryValueRenderer = ({ component }) => {
   const dispatch = useDispatch();
@@ -10,31 +10,32 @@ const QueryValueRenderer = ({ component }) => {
   const query = useSelector(state => state.w3s.queries.list.find(q => q._id === component.props.queryId));
 
   useEffect(() => {
-    if (component.props.queryId) {
+    if (component.props.queryId && query) {
       setLoading(true);
       setError(null);
-      dispatch(fetchQuery(component.props.queryId))
-        .then(() => setLoading(false))
+      dispatch(executeQuery({ endpoint: query.endpoint, query: query.queryString }))
+        .then((action) => {
+          if (action.payload && action.payload.data) {
+            let result = action.payload.data;
+            if (component.props.field) {
+              component.props.field.split('.').forEach(key => {
+                result = result && result[key];
+              });
+            }
+            if (Array.isArray(result) && result.length > 0) {
+              result = result[0];
+            }
+            setValue(result);
+          }
+        })
         .catch(err => {
           setError(err.message);
+        })
+        .finally(() => {
           setLoading(false);
         });
     }
-  }, [dispatch, component.props.queryId]);
-
-  useEffect(() => {
-    if (query && query.result) {
-      try {
-        let result = query.result;
-        if (Array.isArray(result) && component.props.field) {
-          result = result.map(item => item[component.props.field]);
-        }
-        setValue(Array.isArray(result) ? result[0] : result);
-      } catch (err) {
-        setError('Error processing query result');
-      }
-    }
-  }, [query, component.props]);
+  }, [dispatch, component.props.queryId, component.props.field, query]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -53,6 +54,6 @@ const QueryValueRenderer = ({ component }) => {
       {value}
     </div>
   );
-}
+};
 
 export default QueryValueRenderer;
